12c0c4b3734f11614ea462d2cf5d1b9a9499b4de
vishwaskapte <45562090+vishwaskapte@users.noreply.github.com>
2024-01-04 19:59:15 +0530

    Added Sum of Natural Numbers

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@ using ProgrammingCode;
 // Factorial of a Nummber
 Factorial fact = new Factorial();
 fact.CountFactor(100);
+Console.WriteLine($" ");
+
 
 //Check Number is Prime or Not
 CheckPrime chk = new CheckPrime();
 chk.CheckNumberPrimeIsPrimeOrNot(558);
+Console.WriteLine($" ");
+
+
+// Sum of N Natural Numbers
+SumOfNaturalNumbers sm = new SumOfNaturalNumbers();
+Console.WriteLine($"Sum of N Natural Number is = > {sm.SumOfNumbers(4)} ");
---
+++ SumOfNaturalNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingCode
+{
+    public class SumOfNaturalNumbers
+    {
+
+        // Formula is Sum = N * (N + 1) / 2
+
+        public int SumOfNumbers(int n)
+        {
+            return n * (n + 1) / 2;
+        }
+    }
+}
